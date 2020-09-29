d8508cb259664869eaf5302caff33104271dfc3f
Brad Caron <bacaron@iu.edu>
2020-09-29 17:33:38 -0400

    reason: initial commit

--- compile-tractmeasures.py
+++ compile-tractmeasures.py
@@ -7,19 +7,29 @@ import numpy as np
 import os, sys, argparse
 import glob
 
-def concatenateData(tsvs,subject,session,tag):
+def concatenateTractData(tsvs,subject,session,tag):
+
+	tractFiles = glob.glob(tsvs+'/*dwi*')
+	tractNames = [ f.split('.stat.txt')[0].split('.')[-1:][0] for f in tractFiles ]
 
 	# combine tsvs
-	data = pd.concat((pd.read_csv(f,sep='\t',index_col=False) for f in tsvs),ignore_index=True)
-	data['subjectID'] = subject
-	data['sessionID'] = session
-	data['tags'] = tag
+	data = pd.DataFrame([],dtype=object)
+	tmpdata = pd.read_csv(tractFiles[0],sep='\t',index_col=False,header=None)
+	data['measures'] = tmpdata[0]
+	data[tractNames[0]] = tmpdata[1]
+	for tracts in range(len(tractNames)-1):
+		tmpdata = pd.read_csv(tractFiles[tracts+1],sep='\t',index_col=False,header=None)
+		data[tractNames[tracts+1]] = tmpdata[1]
+
+	data['subjectID'] = [ subject for f in range(len(data[tractNames[0]])) ]
+	data['sessionID'] = [ session for f in range(len(data[tractNames[0]])) ]
+	data['tags'] = [ tag for f in range(len(data[tractNames[0]])) ]
 
 	columns = data.columns.tolist()
 	columns = columns[-3:] + columns[:-3]
 	data = data[columns]
 
-	data.to_csv('./tractmeasures/tractmeasure.tsv',sep='\t',index=False)
+	return data
 	
 def main():
 
@@ -34,6 +44,16 @@ def main():
 	tags = [ f['tags'] for f in config['_inputs'] ]
 	tsvs = config['tractmeasures']
 
+	#### create and concatenate dataframes for all tracts into single tsv
+	if len(tsvs) == 1:
+		concat_data = concatenateTractData(tsvs,subjects,sessions,tags)
+	else:
+		concat_data = pd.DataFrame([],dtype=object)
+		for TSVS in range(len(tsvs)):
+			#tmp = concatenateTractData(tsvs[TSVS],subjects[TSVS],sessions[TSVS],tags[TSVS])
+			concat_data = pd.concat([concat_data,concatenateTractData(tsvs[TSVS],subjects[TSVS],sessions[TSVS],tags[TSVS])])
+
+
 	#### set up other inputs ####
 	# set outdir
 	outdir = 'tractmeasures'
@@ -45,9 +65,8 @@ def main():
 		print("making output directory")
 		os.mkdir(outdir)
 
-	#### run command to generate csv structures ####
-	print("concatenating tractmeasures")
-	concatenateData(tsvs,subjects,sessions,tags)
+	#### save concatenated data structure ####
+	concat_data.to_csv('./tractmeasures/tractmeasures.tsv',sep='\t',index=False)
 
 if __name__ == '__main__':
 	main()
